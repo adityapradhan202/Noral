e863e8da495f1d5f19a644a6d211b6af8ba45a06
adityapradhan202 <adityapradhan5060@gmail.com>
2024-12-01 15:30:23 +0530

    sidebar visibility and login system for doctor

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import os
 from PIL import Image
 import time
 import requests
+import json
 
 # Import for db connectivity
 from dbutils import appreqs, dbread
 from apputils.typewriter_effect import stream_data
+
+with open('dummylogin_creds.json', 'r') as file:
+    login_info = json.load(file)
 
 temp_folder = "temp_files"
 os.makedirs(temp_folder, exist_ok=True)
@@ -16,25 +20,32 @@ st.write("Welcome to NORAL: Where Oral Health Meets the Power of Neural Networks
 
 if 'info_submit_status' not in st.session_state:
     st.session_state.info_submit_status = False
+if 'sidebar_display' not in st.session_state:
+    st.session_state.sidebar_display = True
+if 'doctor_login_status' not in st.session_state:
+    st.session_state.doctor_login_status = False
 
-with st.sidebar:
-    name = st.text_input("Enter your name:")
-    age = st.number_input("Enter your age:", value=None)
-    address = st.text_input("Enter address:")
-    phone = st.text_input("Enter phone number:")
-    gender = st.radio(label="Choose your gender:",
-                      options=["Male", "Female", "Others"], index=None)
-    
-    submit = st.button(label="Submit information")
-    if submit:
-        if name and age and address and phone and gender:
-            st.session_state.info_submit_status = True
-            st.success("Information successfully submited!")
-        else:
-            st.warning("Enter all the fields in order to proceed!")
+if st.session_state.sidebar_display==True:
+    with st.sidebar:
+        name = st.text_input("Enter your name:")
+        age = st.number_input("Enter your age:", value=None)
+        address = st.text_input("Enter address:")
+        phone = st.text_input("Enter phone number:")
+        gender = st.radio(label="Choose your gender:",
+                        options=["Male", "Female", "Others"], index=None)
+        
+        submit = st.button(label="Submit information")
+        if submit:
+            if name and age and address and phone and gender:
+                st.session_state.info_submit_status = True
+                st.success("Information successfully submited!")
+            else:
+                st.warning("Enter all the fields in order to proceed!")
     
 tab1, tab2 = st.tabs(tabs=["Patient portal", "Doctor's dashboard"])
 with tab1:
+    st.session_state.sidebar_display = True 
+
     uploaded_image = st.file_uploader(label='Upload an image of your teeth', type=['jpg', 'jpeg', 'png'])
     description = st.text_area(
         label="Describe your problem here(Max 300 characters)"
@@ -95,12 +106,44 @@ with tab1:
             st.warning("First fill all the information on the sidebar given on the left in order to make the AI system examine your problem!")
 
 with tab2:
-    st.write("Doctor's login here...")
-    st.write("This part is under development!")
+    if st.session_state.doctor_login_status==False:
+        st.write("Welcome to the doctor's portal. In order to access the dashboard, you must login!")
+    
+        dusername = st.text_input(label="Enter username:", value=None)
+        dpass = st.text_input(label="Enter password:", value=None, type='password')
+        dlogin_btn = st.button("Login")
+        if dlogin_btn:
+            if dusername and dpass:  
+                if login_info["username"] == dusername and login_info["password"] == dpass:
+                    st.success("Login successful! You can procced now!")
+                    with st.spinner(text="Wait for a second..."):
+                        time.sleep(2)
+                        st.session_state.doctor_login_status = True
+                        st.session_state.sidebar_display = False
+                        st.rerun(scope="app")
+                else:
+                    st.warning("Invalid creds! Enter correct username and password")
+            else:
+                st.warning("Enter both the username and password to login!")
+            
+    else:
+        st.session_state.sidebar_display = False
 
-    dash = st.button(label="See dashboard", type='secondary')
-    if dash:
-        st.write(dbread.display_db())
+        # Sidebar in case of the doctor
+        with st.sidebar:
+            st.success("Login successful!")
+            st.write("Doctor's sidebar")
+            logout_btn = st.button(label="Logout", type='primary')
+            if logout_btn:
+                st.session_state.doctor_login_status = False
+                st.session_state.sidebar_display = True
+                st.rerun(scope="app")
+
+
+        
+        dash = st.button(label="See dashboard", type='secondary')
+        if dash:
+            st.write(dbread.display_db())
 
     
 
